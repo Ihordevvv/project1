bca6d208d96706e76ce5f79caab6a65e89ec4eea
Ihor <Igor0210th@gmail.com>
2023-02-13 18:17:31 +0200

    cycles

--- js/script.js
+++ js/script.js
@@ -6,12 +6,52 @@ const personalMovieDB = {count: '', movies: {}, actors: {}, genres: [], privat: 
 
 personalMovieDB.count = numberOfFilms;
 
-let a = prompt('Последний просмотренный фильм?', ''),
-    b = prompt('Как вы его оцените?', ''),
-    c = prompt('Последний просмотренный фильм?', ''),
-    d = prompt('Как вы его оцените?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+
+//for (let i = 0; i < 2; i++) {
+//	let a = prompt('Последний просмотренный фильм?', ''),
+//		b = prompt('Как вы его оцените?', '');
+//	if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//		personalMovieDB.movies[a] = b;
+//	} else {
+//		i--;
+//	}
+//}
+
+//let i = 1;
+//cycle:
+//while (i < 3) {
+//	let a = prompt('Последний просмотренный фильм?', ''),
+//		b = prompt('Как вы его оцените?', '');
+//		if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//		personalMovieDB.movies[a] = b;
+//		} else {
+//		continue cycle;
+//		}
+//		i++;
+//}
+let i = 1;
+cycle:
+do {
+	let a = prompt('Последний просмотренный фильм?', ''),
+		b = prompt('Как вы его оцените?', '');
+	if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+		personalMovieDB.movies[a] = b;
+	}else {
+		continue cycle;
+	}
+	i++;
+} while (i < 3);
+
+
+
+if (personalMovieDB.count < 10) {
+	console.log('Malo');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+	console.log('Norm');
+} else if (personalMovieDB.count >= 50) {
+	console.log('Ebanat');
+}
 
 console.log(personalMovieDB);
+
